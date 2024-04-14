16e26fe75b2c5821135c630d06645596d4614605
Davide Caroselli <davide@modernmt.eu>
2024-04-14 11:38:43 +0200

    Code reformat

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export {
-    Jsonthis, JsonthisOptions, Json, JsonField, JsonFieldOptions, JsonFieldFunction, CircularReferenceError
+    Jsonthis,
+    JsonthisOptions,
+    Json,
+    JsonField,
+    JsonFieldOptions,
+    JsonFieldFunction,
+    CircularReferenceError
 } from './jsonthis';
